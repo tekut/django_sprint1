a36ec0013ee484b3a91a8aed8e79f46f74817a55
Aleksandr Tekutev <tekutiev.sasha@gmail.com>
2023-05-09 13:07:16 +0100

    Initial upload test

---
+++ blogicum/pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def about(request):
+    template_name = 'pages/about.html'
+    return render(request, template_name)
+
+
+def rules(request):
+    template_name = 'pages/rules.html'
+    return render(request, template_name)
